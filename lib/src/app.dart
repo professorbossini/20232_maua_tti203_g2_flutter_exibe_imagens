cdb07b4939677d7e7f6870f198eccb3909699708
Rodrigo Bossini <professorbossini@gmail.com>
2023-08-30 13:03:52 -0300

    flutter: fim da apostila 01

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  State<App> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int numeroImagens = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$numeroImagens'),
         appBar: AppBar(title: const Text("Minhas Imagens")),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
-          onPressed: () => print('Estou no arquivo app.dart'),
+          onPressed: () {
+            //numeroImagens++;
+            setState(() {
+              numeroImagens++;
+            });
+          },
         ),
       ),
     );
